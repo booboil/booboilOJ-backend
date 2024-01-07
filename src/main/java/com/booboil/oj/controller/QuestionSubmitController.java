ce7ab06f766c32f8b659c88406dc2c0d3ac23a04
张新荣 <2914424377@qq.com>
2024-01-07 19:50:28 +0800

    提交判题服务bug todo

--- src/main/java/com/booboil/oj/controller/QuestionSubmitController.java
+++ src/main/java/com/booboil/oj/controller/QuestionSubmitController.java
@@ -1,63 +1,63 @@
-//package com.booboil.oj.controller;
-//
-//import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-//import com.booboil.oj.common.BaseResponse;
-//import com.booboil.oj.common.ErrorCode;
-//import com.booboil.oj.common.ResultUtils;
-//import com.booboil.oj.exception.BusinessException;
-//import com.booboil.oj.model.dto.questionsubmit.QuestionSubmitAddRequest;
-//import com.booboil.oj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
-//import com.booboil.oj.model.entity.QuestionSubmit;
-//import com.booboil.oj.model.entity.User;
-//import com.booboil.oj.model.vo.QuestionSubmitVO;
-//import com.booboil.oj.service.QuestionSubmitService;
-//import com.booboil.oj.service.UserService;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import javax.annotation.Resource;
-//import javax.servlet.http.HttpServletRequest;
-//
-///**
-// * 题目提交接口
-// *
-// * @author <a href="https://github.com/booboil">程序员booboil</a>
-// *
-// */
-//@RestController
-//@RequestMapping("/question_submit")
-//@Slf4j
-////@Deprecated
-//public class QuestionSubmitController {
-//
-//    @Resource
-//    private QuestionSubmitService questionSubmitService;
-//
-//    @Resource
-//    private UserService userService;
-//
-//    /**
-//     * 提交题目
-//     *
-//     * @param questionSubmitAddRequest
-//     * @param request
-//     * @return 提交记录的 id
-//     */
-//    @PostMapping("/")
-//    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
-//                                               HttpServletRequest request) {
-//        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
-//            throw new BusinessException(ErrorCode.PARAMS_ERROR);
-//        }
-//        // 登录才能提交
-//        final User loginUser = userService.getLoginUser(request);
-//        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
-//        return ResultUtils.success(questionSubmitId);
-//    }
-//
+package com.booboil.oj.controller;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.booboil.oj.common.BaseResponse;
+import com.booboil.oj.common.ErrorCode;
+import com.booboil.oj.common.ResultUtils;
+import com.booboil.oj.exception.BusinessException;
+import com.booboil.oj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.booboil.oj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
+import com.booboil.oj.model.entity.QuestionSubmit;
+import com.booboil.oj.model.entity.User;
+import com.booboil.oj.model.vo.QuestionSubmitVO;
+import com.booboil.oj.service.QuestionSubmitService;
+import com.booboil.oj.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 题目提交接口
+ *
+ * @author <a href="https://github.com/booboil">程序员booboil</a>
+ *
+ */
+@RestController
+@RequestMapping("/question_submit")
+@Slf4j
+//@Deprecated
+public class QuestionSubmitController {
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 提交题目
+     *
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return 提交记录的 id
+     */
+    @PostMapping("/")
+    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+                                               HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能提交
+        final User loginUser = userService.getLoginUser(request);
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(questionSubmitId);
+    }
+}
 //    /**
 //     * 提交题目
 //     *
