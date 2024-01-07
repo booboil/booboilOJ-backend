d8be1246d63280fd6610a64b5e926ccc7a75baa2
张新荣 <2914424377@qq.com>
2024-01-07 19:11:41 +0800

    用户提交相应接口

--- src/main/java/com/booboil/oj/model/vo/QuestionSubmitVO.java
+++ src/main/java/com/booboil/oj/model/vo/QuestionSubmitVO.java
@@ -1,8 +1,8 @@
 package com.booboil.oj.model.vo;
 
 import cn.hutool.json.JSONUtil;
-import com.booboil.oj.judge.codesandbox.model.JudgeInfo;
 import com.booboil.oj.model.entity.QuestionSubmit;
+import com.booboil.oj.model.dto.questionsubmit.JudgeInfo;
 import lombok.Data;
 import org.springframework.beans.BeanUtils;
 
