b38be26eb0a7ac643b729b1c3a9f07e02655e9b9
张新荣 <2914424377@qq.com>
2024-01-10 17:24:33 +0800

    提交题目接口（分页）

--- src/main/java/com/booboil/oj/controller/QuestionController.java
+++ src/main/java/com/booboil/oj/controller/QuestionController.java
@@ -10,14 +10,18 @@ import com.booboil.oj.constant.UserConstant;
 import com.booboil.oj.exception.BusinessException;
 import com.booboil.oj.exception.ThrowUtils;
 import com.booboil.oj.model.dto.question.*;
+import com.booboil.oj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.booboil.oj.model.dto.questionsubmit.QuestionSubmitQueryRequest;
 import com.booboil.oj.model.entity.Question;
+import com.booboil.oj.model.entity.QuestionSubmit;
 import com.booboil.oj.model.entity.User;
+import com.booboil.oj.model.vo.QuestionSubmitVO;
 import com.booboil.oj.model.vo.QuestionVO;
 import com.booboil.oj.service.QuestionService;
+import com.booboil.oj.service.QuestionSubmitService;
 import com.booboil.oj.service.UserService;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,6 +47,9 @@ public class QuestionController {
     private UserService userService;
 
     private final static Gson GSON = new Gson();
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
 
 
     /**
@@ -289,6 +296,45 @@ public class QuestionController {
         }
         boolean result = questionService.updateById(question);
         return ResultUtils.success(result);
+    }
+
+    /**
+     * 提交题目
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/question_submit/do")
+    public BaseResponse<Long> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+                                               HttpServletRequest request ) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能提交
+        final User loginUser = userService.getLoginUser(request);
+        long questionSubmitId = questionSubmitService.doQuestionSubmit(questionSubmitAddRequest, loginUser);
+        return ResultUtils.success(questionSubmitId);
+
+    }
+
+    /**
+     * 分页获取题目提交列表（除了管理员外，普通用户只能看到非答案、提交代码等公开信息）
+     *
+     * @param questionSubmitQueryRequest
+     * @param request
+     * @return
+     */
+    @PostMapping("/question_submit/list/page")
+    public BaseResponse<Page<QuestionSubmitVO>> listQuestionSubmitByPage(@RequestBody QuestionSubmitQueryRequest questionSubmitQueryRequest,
+                                                                         HttpServletRequest request) {
+        long current = questionSubmitQueryRequest.getCurrent();
+        long size = questionSubmitQueryRequest.getPageSize();
+        // 从数据库中查询原始的题目提交分页信息
+        Page<QuestionSubmit> questionSubmitPage = questionSubmitService.page(new Page<>(current, size),
+                questionSubmitService.getQueryWrapper(questionSubmitQueryRequest));
+        final User loginUser = userService.getLoginUser(request);
+        // 返回脱敏信息
+        return ResultUtils.success(questionSubmitService.getQuestionSubmitVOPage(questionSubmitPage, loginUser));
     }
 
 }
