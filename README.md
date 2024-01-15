f4db209899ef060d8ca0b436ebeed6d9c283176d
张新荣 <2914424377@qq.com>
2024-01-15 21:35:34 +0800

    修改md

--- README.md
+++
@@ -1,5 +0,0 @@
-# Sprin
-
-> 作者：[程序员booboil](https://github.com/booboil)
-
-
--- src/main/java/com/booboil/oj/BooboilOJApplication.java
+++ src/main/java/com/booboil/oj/BooboilOJApplication.java
@@ -24,3 +24,4 @@ public class BooboilOJApplication {
         SpringApplication.run(BooboilOJApplication.class, args);
     }
 }
+
