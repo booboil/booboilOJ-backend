68fb3216f5effe5ae66309d6951c545aee81a0a4
张新荣 <2914424377@qq.com>
2024-01-24 17:28:09 +0800

    项目redis配置信息

--- src/main/java/com/booboil/oj/BooboilOJApplication.java
+++ src/main/java/com/booboil/oj/BooboilOJApplication.java
@@ -14,7 +14,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * 
  */
 // todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+//@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan("com.booboil.oj.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
